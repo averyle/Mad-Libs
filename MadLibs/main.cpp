fbefdf96b7d5c87da2183e5efa0ddff292fe296b
Avery Le <ngangianggg@gmail.com>
2021-01-22 11:22:54 -0800

    project 1 updated

--- MadLibs/main.cpp
+++ MadLibs/main.cpp
@@ -1,5 +1,52 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello, World\n";
+	std::cout << "Hello!\n" << "Give me an adjective\n";
+	std::string word1;
+	std::cin >> word1;
+	std::cout << "Give me a nationality\n";
+	std::string word2;
+	std::cin >> word2;
+	std::cout << "Give me a person's name\n";
+	std::string word3;
+	std::cin >> word3;
+	std::cout << "Give me a noun\n";
+	std::string word4;
+	std::cin >> word4; 
+	std::cout << "Give me an adjective\n";
+	std::string word5;
+	std::cin >> word5; 
+	std::cout << "Give me a noun\n";
+	std::string word6;
+	std::cin >> word6; 
+	std::cout << "Give me an adjective\n";
+	std::string word7;
+	std::cin >> word7;
+	std::cout << "Give me an adjective\n";
+	std::string word8;
+	std::cin >> word8;
+	std::cout << "Give me a plural noun\n";
+	std::string word9;
+	std::cin >> word9;
+	std::cout << "Give me a noun\n";
+	std::string word10;
+	std::cin >> word10;
+	std::cout << "Give me a number\n";
+	std::string word11;
+	std::cin >> word11;
+	std::cout << "Give me a shape\n";
+	std::string word12;
+	std::cin >> word12;
+	std::cout << "Give me a one-word kind of food\n";
+	std::string word13;
+	std::cin >> word13;
+	std::cout << "Give me a one-word kind of food\n";
+	std::string word14;
+	std::cin >> word14;
+	std::cout << "Give me a number\n";
+	std::string word15;
+	std::cin >> word15;
+
+	std::cout << "Pizza was invented by a " << word1 << " " << word2 << " chef named " << word3 << ". To make pizza, you need to take a lump of " << word4 << ", and make a thin, round " << word5 << " " << word6 << ". Then you cover it with " << word7 << " sauce, " << word8 << " cheese, and fresh chopped " << word9 << ". Next you have to bake it in a very hot " << word10 << ". When it is done, cut it into " << word11 << " " << word12 << "s. Some kids like " << word13 << " pizza the best, but my favorite is the " << word14 << " pizza. If I could, I would eat pizza " << word15 << " times a day!\n";
+
 }
